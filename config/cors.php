135944707ab3f22794c3353687013d1f3ab91fd9
Emmanuel Perez <noel@Noel.local>
2022-09-14 14:22:25 +0800

    Updated cors config

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api/v1/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api/v1/*', 'api'],
 
     'allowed_methods' => ['*'],
 
