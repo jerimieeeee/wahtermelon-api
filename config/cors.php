58bf030a78d5c306face7df71492fcd346de5fe8
Emmanuel Perez <noel@Noel.local>
2022-09-14 13:45:27 +0800

    Updated cords config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'https://apiwahtermelon-staging.wah.ph', 'http://apiwahtermelon-staging.wah.ph'],
+    'allowed_origins' => ['*', 'https://apiwahtermelon-staging.wah.ph'],
 
     'allowed_origins_patterns' => [],
 
