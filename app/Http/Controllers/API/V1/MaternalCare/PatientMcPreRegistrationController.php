39400aade38fac85b341997dd942d684d4931212
Emmanuel Perez <noel@Noel.local>
2022-10-13 16:45:56 +0800

    Updated maternal care module

--- app/Http/Controllers/API/V1/MaternalCare/PatientMcPreRegistrationController.php
+++ app/Http/Controllers/API/V1/MaternalCare/PatientMcPreRegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\V1\MaternalCare;
 use App\Http\Controllers\Controller;
 use App\Models\V1\MaternalCare\PatientMc;
 use App\Models\V1\MaternalCare\PatientMcPostRegistration;
+use App\Models\V1\MaternalCare\PatientMcPreRegistration;
 use App\Models\V1\Patient\Patient;
 use Illuminate\Http\Request;
 
@@ -32,9 +33,19 @@ class PatientMcPreRegistrationController extends Controller
         return $a = PatientMcPostRegistration::with(['patientMc'])->first();
         //return $a->patientMcPostRegistration;
         return $mc = PatientMc::with('patientMcPostRegistration')->first();*/
-        return $mc = PatientMc::withWhereHas('postRegister', function($query) {
+        /*return $mc = PatientMc::withWhereHas('postRegister', function($query) {
             $query->where('end_pregnancy', false);
-        })->first();
+        })->get();*/
+        return $mc = PatientMc::addSelect([
+                'post_registration' => PatientMcPostRegistration::select('post_registration_date')
+                ->whereColumn('patient_mc_id', 'patient_mc.id')
+                ->where('end_pregnancy', false),
+                'pre_registration' => PatientMcPreRegistration::select('pre_registration_date')
+                ->whereColumn('patient_mc_id', 'patient_mc.id')
+        ])
+        ->havingRaw('(pre_registration IS NULL AND post_registration IS NOT NULL) OR (pre_registration IS NOT NULL AND post_registration IS NULL) OR (pre_registration IS NOT NULL AND post_registration IS NOT NULL) OR (pre_registration IS NULL AND post_registration IS NULL)')
+            ->latest('post_registration', 'pre_registration')
+            ->first();
         /*if(!$mc){
             return $mc = PatientMc::create($request->all());
         }*/
