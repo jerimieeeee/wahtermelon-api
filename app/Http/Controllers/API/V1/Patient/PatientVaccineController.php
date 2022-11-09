02fb68b3c3c49566de2d62e9dbbd8c675a9555e7
Jerimie <eimirejian05@gmail.com>
2022-11-09 05:19:27 +0000

    fix vaccine not saving same vaccine id

--- app/Http/Controllers/API/V1/Patient/PatientVaccineController.php
+++ app/Http/Controllers/API/V1/Patient/PatientVaccineController.php
@@ -48,8 +48,7 @@ class PatientVaccineController extends Controller
             $vaccine = $request->input('vaccine_id');
             $vaccine_array = [];
             foreach($vaccine as $key => $value){
-                $data = PatientVaccine::firstOrNew(['patient_id' => $request->input('patient_id') , 'vaccine_id' => $value]);
-                $data->user_id = $request->input('user_id');
+                $data = PatientVaccine::create(['patient_id' => $request->input('patient_id') , 'vaccine_id' => $value,'user_id' => $request->input('user_id')]);
                 $data->status_id = $request->input('status_id')[$key] == null ? null : ($request->input('status_id')[$key]);
                 $data->vaccine_id = $value;
                 $data->vaccine_date = $request->input('vaccine_date')[$key] == null ? null : Carbon::parse($request->input('vaccine_date')[$key])->format('Y-m-d');
