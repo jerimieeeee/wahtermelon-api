0bc14a405f3a427105eb2b8efcb2ba561c7ae10d
vagrant <vagrant@homestead>
2022-11-20 01:43:23 +0000

    Updated pwd types

--- database/seeders/LibPwdTypeSeeder.php
+++ database/seeders/LibPwdTypeSeeder.php
@@ -16,15 +16,16 @@ class LibPwdTypeSeeder extends Seeder
     public function run()
     {
         LibPwdType::upsert([
-            ['code' => 'CID', 'sequence' => 1, 'type_desc' => 'Chronic Illness'],
-            ['code' => 'CD', 'sequence' => 2,  'type_desc' => 'Communication Disability'],
-            ['code' => 'LD', 'sequence' => 3,  'type_desc' => 'Learning Disability'],
+            ['code' => 'CN', 'sequence' => 1, 'type_desc' => 'Cancer (RA 11215)'],
+            ['code' => 'CID', 'sequence' => 7, 'type_desc' => 'Rare Disease (RA 10747)'], //From Chronic Illness to Rare Disease (RA 10747)
+            ['code' => 'CD', 'sequence' => 2,  'type_desc' => 'Deaf or Hard Hearing'], //From Communication Disability to Deaf or hard Hearing
+            ['code' => 'LD', 'sequence' => 3,  'type_desc' => 'Intellectual Disability'], //From Learning Disability to Intellectual Disability
             ['code' => 'MD', 'sequence' => 4,  'type_desc' => 'Mental Disability'],
-            ['code' => 'OD', 'sequence' => 5,  'type_desc' => 'Orthopedic Disability'],
-            ['code' => 'PD', 'sequence' => 6,  'type_desc' => 'Psychosocial Disability'],
-            ['code' => 'VD', 'sequence' => 7,  'type_desc' => 'Visual Disability'],
-            ['code' => 'UN', 'sequence' => 8,  'type_desc' => 'Undefined'],
+            ['code' => 'OD', 'sequence' => 6,  'type_desc' => 'Physical Disability (Orthopedic)'], //From Orthopedic Disability to Physical Disability (Orthopedic)
+            ['code' => 'PD', 'sequence' => 5,  'type_desc' => 'Psychosocial DIsability Speech and Language Impairment'], //From Psychosocial Disability to Psychosocial DIsability Speech and Language Impairment
+            ['code' => 'VD', 'sequence' => 8,  'type_desc' => 'Visual Disability'],
+            ['code' => 'UN', 'sequence' => 9,  'type_desc' => 'Undefined'],
             ['code' => 'NA', 'sequence' => 0,  'type_desc' => 'Not Applicable'],
-        ], ['type_desc']);
+        ], ['code']);
     }
 }
