e76192d431be0f970b13bec97f412061730742ce
Jerimie <eimirejian05@gmail.com>
2022-12-09 07:59:08 +0000

    Fix testing on consult module

--- app/Http/Requests/API/V1/Consultation/ConsultNotesRequest.php
+++ app/Http/Requests/API/V1/Consultation/ConsultNotesRequest.php
@@ -31,6 +31,8 @@ class ConsultNotesRequest extends FormRequest
             'complaint' => 'nullable',
             'history' => 'nullable',
             'physical_exam' => 'nullable',
+            'idx_remark' => 'nullable',
+            'fdx_remark' => 'nullable',
             'plan' => 'nullable',
         ];
     }
@@ -56,6 +58,14 @@ class ConsultNotesRequest extends FormRequest
             ],
             'physicl_exam' => [
                 'description' => 'remarks of consult_notes_pes',
+                'example' => fake()->sentence(),
+            ],
+            'idx_remark' => [
+                'description' => 'remarks of initial diagnosis',
+                'example' => fake()->sentence(),
+            ],
+            'fdx_remark' => [
+                'description' => 'remarks of final diagnosis',
                 'example' => fake()->sentence(),
             ],
             'plan' => [
