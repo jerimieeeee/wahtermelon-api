a1a401878a0a902122644f5b5d885f0c8f25f50b
Jerimie <eimirejian05@gmail.com>
2023-01-10 13:10:54 +0000

    urinalysis resource

--- app/Http/Resources/API/V1/Konsulta/LaboratoryUrinalysisResource.php
+++ app/Http/Resources/API/V1/Konsulta/LaboratoryUrinalysisResource.php
@@ -14,6 +14,36 @@ class LaboratoryUrinalysisResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            '_attributes' => [
+                'pReferralFacility' => "",
+                'pLabDate' => $this->laboratory_date?? "",
+                'pGravity' => $this->gravity?? "",
+                'pAppearance' => $this->appearance?? "",
+                'pColor' => $this->appearance?? "",
+                'pGlucose' => $this->mch?? "",
+                'pRbCells' => $this->mch?? "",
+                'pWbCells' =>  $this->mch?? "",
+                'pBacteria' => $this->mch?? "",
+                'pCrystals' => $this->mch?? "",
+                'pBladderCell' => $this->mcv?? "",
+                'pSquamousCell' => $this->wbc?? "",
+                'pTubularCell' => "",
+                'pBroadCasts' => "",
+                'pEpithelialCast' => $this->neutrophils?? "",
+                'pGranularCast' =>  "",
+                'pHyalineCast' => $this->lymphocytes?? "",
+                'pRbcCast' => $this->monocytes?? "",
+                'pWaxyCast' => $this->eosinophils?? "",
+                'pWcCast' => $this->basophils?? "",
+                'pAlbumin' => $this->platelets?? "",
+                'pPusCells' => $this->platelets?? "",
+                'pDateAdded' => isset($this->created_at) ? $this->created_at->format('Y-m-d') : "",
+                'pStatus' => $this->lab_status_code?? "",
+                'pDiagnosticLabFee' => "",
+                'pReportStatus' => "U",
+                'pDeficiencyRemarks' => ""
+            ],
+        ];
     }
 }
