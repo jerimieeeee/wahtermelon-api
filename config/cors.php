6dceef3b7585a1ec280fc509b75f096f12f9a726
Emmanuel Perez <noel@Noel.local>
2023-01-24 15:11:10 +0800

    Updated cors config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'https://apiwahtermelon-staging.wah.ph'],
+    'allowed_origins' => ['http://localhost:4200', 'https://apiwahtermelon-staging.wah.ph', 'https://wahtermelon-staging.wah.ph', 'https://wahtermelon.wah.ph'],
 
     'allowed_origins_patterns' => [],
 
