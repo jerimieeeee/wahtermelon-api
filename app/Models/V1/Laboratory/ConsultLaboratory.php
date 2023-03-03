484e0072f4a7aa7c8dda22522849d5f59c7eba77
Emmanuel Perez <noel@Noel.local>
2023-03-03 16:07:16 +0800

    Fixed merge conflicts

--- app/Models/V1/Laboratory/ConsultLaboratory.php
+++ app/Models/V1/Laboratory/ConsultLaboratory.php
@@ -176,5 +176,11 @@ class ConsultLaboratory extends Model
     public function microscopy()
     {
         return $this->hasOne(ConsultLaboratoryMicroscopy::class, 'request_id', 'id');
+
+    }
+
+    public function getRelatedModel($model)
+    {
+        return $this->$model();
     }
 }
