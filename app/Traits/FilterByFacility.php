8460f5b84d0b75a8b43d4f713295c26c22635a43
Emmanuel Perez <noel@Noel.local>
2023-03-16 08:50:49 +0800

    Fixed filter by facility

--- app/Traits/FilterByFacility.php
+++ app/Traits/FilterByFacility.php
@@ -17,7 +17,7 @@ trait FilterByFacility
         if (auth()->check()) {
             static::addGlobalScope('facility', function (Builder $builder) {
                 if(Schema::hasColumn($builder->getQuery()->from, 'facility_code') && isset(auth()->user()->facility_code)){
-                    $builder->where('facility_code', auth()->user()->facility_code);
+                    $builder->where($builder->getQuery()->from.'.facility_code', auth()->user()->facility_code);
                 }
             });
         }
