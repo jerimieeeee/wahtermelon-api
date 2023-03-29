ad20a1fde5b9e8b3ca5781e322136d8eecca7c5d
Emmanuel Perez <noel@Noel.local>
2023-03-29 12:59:12 +0800

    Format files using pint

--- app/Http/Controllers/API/Authorization/RoleController.php
+++ app/Http/Controllers/API/Authorization/RoleController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API\Authorization;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\V1\Patient\Patient;
 use Silber\Bouncer\BouncerFacade as Bouncer;
 
 class RoleController extends Controller
@@ -12,17 +14,27 @@ class RoleController extends Controller
      */
     public function addRole()
     {
-        $admin = Bouncer::role()->firstOrCreate([
-            'name' => 'admin',
-            'title' => 'Administrator',
-        ]);
+//        $admin = Bouncer::role()->firstOrCreate([
+//            'name' => 'admin',
+//            'title' => 'Administrator',
+//        ]);
+//
+//        $ban = Bouncer::ability()->firstOrCreate([
+//            'name' => 'ban-users',
+//            'title' => 'Ban users',
+//        ]);
+//
+//        Bouncer::allow($admin)->to($ban);
+        //$user = User::whereIs('admin')->get();
 
-        $ban = Bouncer::ability()->firstOrCreate([
-            'name' => 'ban-users',
-            'title' => 'Ban users',
-        ]);
+        return auth()->user()->getAbilities();
 
-        Bouncer::allow($admin)->to($ban);
+        return Bouncer::is(auth()->user())->an('admin');
+        Bouncer::allow('admin')->to('ban-users');
+        Bouncer::assign('admin')->to(auth()->id());
+        Bouncer::allow(auth()->id())->to('ban-users');
+        Bouncer::allow(auth()->id())->to('edit', Patient::class);
+        Bouncer::allow(auth()->id())->toOwn(Patient::class);
 
         return 'OK';
     }
