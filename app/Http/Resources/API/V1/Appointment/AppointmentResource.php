fcb0cac1bbc922f9beb79dece0dd9b4dcaf4dd1a
Jerimie Garcia <je@Jerimie.local>
2023-04-18 09:41:05 +0800

    merge api-documentation branch

--- app/Http/Resources/API/V1/Appointment/AppointmentResource.php
+++ app/Http/Resources/API/V1/Appointment/AppointmentResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources\API\V1\Appointment;
 
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -16,46 +15,14 @@ class AppointmentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id ?? $this->id,
-            'facility_code' => $this->when(! $this->relationLoaded('facility'), $this->facility_code),
-            'facility' => $this->whenLoaded('facility'),
             'patient_id' => $this->when(! $this->relationLoaded('patient'), $this->patient_id),
             'patient' => $this->whenLoaded('patient'),
-            'user_id' => $this->when(! $this->relationLoaded('user'), $this->user_id),
-            'user' => $this->whenLoaded('user'),
-            'appointment_code' => $this->when(! $this->relationLoaded('appointment'), $this->appointment_code),
-            'appointment' => $this->whenLoaded('appointment'),
-            'appointment_date' => Carbon::parse($this->appointment_date)->format('Y-m-d H:i:s'),
-            'deleted_at' => $this->deleted_at?->format('Y-m-d H:i:s'),
-            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
-            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+            'name' => $this->name,
+            'facility_code' => $this->when(! $this->relationLoaded('facility'), $this->facility_code),
+            'facility' => $this->whenLoaded('facility'),
+            'appointment_desc' => $this->appointment_desc,
+            'modules' => $this->modules,
+            'appointment_date' => $this->appointment_date?->format('Y-m-d'),
         ];
-
-//        $appointments = [];
-//
-//        foreach ($this->collection as $appointment) {
-//            $appointments[] = [
-//                'id' => $appointment->id,
-//                'patient_id' => $appointment->patient_id,
-//                'appointment_code' => $appointment->appointment_code,
-//                'appointment_date' => $appointment->appointment_date,
-//                'created_at' => $appointment->created_at,
-//                'updated_at' => $appointment->updated_at,
-//            ];
-//        }
-//
-//        return [
-//            'data' => $appointments,
-//            'links' => [
-//                'self' => $this->url($request->input('page', 1)),
-//            ],
-//            'meta' => [
-//                'total' => $this->total(),
-//                'per_page' => $this->perPage(),
-//                'current_page' => $this->currentPage(),
-//                'last_page' => $this->lastPage(),
-//                'path' => $this->path(),
-//            ],
-//        ];
     }
 }
