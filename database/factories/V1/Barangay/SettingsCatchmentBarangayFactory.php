9dbce887d64ec3dbcaa212d44c93c202a15aa7a2
Emmanuel Perez <noel@Noel.local>
2023-05-09 08:31:59 +0800

    Added catchment barangay factory

--- database/factories/V1/Barangay/SettingsCatchmentBarangayFactory.php
+++ database/factories/V1/Barangay/SettingsCatchmentBarangayFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories\V1\Barangay;
 
+use App\Models\User;
+use App\Models\V1\PSGC\Barangay;
+use App\Models\V1\PSGC\Facility;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +20,13 @@ class SettingsCatchmentBarangayFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'facility_code' => fake()->randomElement(Facility::pluck('code')->toArray()),
+            'user_id' => fake()->randomElement(User::pluck('id')->toArray()),
+            'year' => fake()->year('now'),
+            'barangay_code' => fake()->randomElement(Barangay::pluck('code')->toArray()),
+            'population' => fake()->randomNumber(),
+            'household' => fake()->randomNumber(),
+            'zod' => fake()->boolean(),
         ];
     }
 }
