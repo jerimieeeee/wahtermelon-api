2215b1834e8cbfb1df01b84596ba91e50d9c0f7c
Mark Santos <christian.santos37@gmail.com>
2023-05-11 10:45:59 +0800

    add deferral reason library

---
+++ database/seeders/LibGbvDeferralReasonSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\V1\Libraries\LibGbvDeferralReason;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LibGbvDeferralReasonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        LibGbvDeferralReason::upsert([
+            ['id' => 1, 'desc' => 'Child upset', 'sequence' => 1],
+            ['id' => 2, 'desc' => 'Not disclosing', 'sequence' => 2],
+            ['id' => 3, 'desc' => 'Previous statement was already taken', 'sequence' => 3]
+        ], ['id']);
+    }
+}
