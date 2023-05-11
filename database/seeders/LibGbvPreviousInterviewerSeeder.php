79cb555b5d9e28a6252ccf10a56ae1d92f9e0f7e
Mark Santos <christian.santos37@gmail.com>
2023-05-11 10:46:35 +0800

    add gbv previous interviewer for deferrals

---
+++ database/seeders/LibGbvPreviousInterviewerSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\V1\Libraries\LibGbvPreviousInterviewer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LibGbvPreviousInterviewerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        LibGbvPreviousInterviewer::upsert([
+            ['id' => 1, 'desc' => 'NBI', 'sequence' => 1],
+            ['id' => 2, 'desc' => 'PNP', 'sequence' => 2],
+            ['id' => 3, 'desc' => 'DSWD', 'sequence' => 3],
+            ['id' => 4, 'desc' => 'Other', 'sequence' => 4],
+            // ['id' => 4, 'desc' => 'Unknown', 'sequence' => 4],
+        ], ['id']);
+    }
+}
