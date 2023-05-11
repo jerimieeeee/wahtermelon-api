8cda78a27b26904bbafc27bdcd1a992ee6549d57
Mark Santos <christian.santos37@gmail.com>
2023-05-11 11:37:02 +0800

    remove location relationship

--- app/Http/Controllers/API/V1/GenderBasedViolence/PatientGbvFamilyCompositionController.php
+++ app/Http/Controllers/API/V1/GenderBasedViolence/PatientGbvFamilyCompositionController.php
@@ -36,7 +36,7 @@ class PatientGbvFamilyCompositionController extends Controller
      */
     public function index(Request $request): ResourceCollection
     {
-        $query = PatientGbvFamilyComposition::query()->with(['patientGbv', 'location'])
+        $query = PatientGbvFamilyComposition::query()->with(['patientGbv', 'relation'])
             ->when(isset($request->patient_id), function ($query) use ($request) {
                 return $query->wherePatientId($request->patient_id);
             });
