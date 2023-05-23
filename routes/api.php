328dabd5bab1173156aef43dfe6b778b3cd14019
Mark Santos <christian.santos37@gmail.com>
2023-05-24 07:59:56 +0800

    add route to show list of gbv users MD, MSWDO, WCPD

--- routes/api.php
+++ routes/api.php
@@ -810,7 +810,7 @@ Route::prefix('v1')->group(function () {
                 Route::post('patient-gbv-conference-mitigating-factor', 'store');
                 Route::put('patient-gbv-conference-mitigating-factor/{patientGbvConfMitigatingFactor}', 'update');
             });
-        Route::controller(\App\Http\Controllers\API\V1\GenderBasedViolence\patientGbvConRecommendationController::class)
+        Route::controller(\App\Http\Controllers\API\V1\GenderBasedViolence\PatientGbvConRecommendationController::class)
             ->middleware(('auth:api'))
             ->group(function () {
                 Route::get('patient-gbv-conference-recommendation', 'index');
@@ -865,6 +865,11 @@ Route::prefix('v1')->group(function () {
                 Route::get('patient-gbv-consult-visit', 'index');
                 Route::post('patient-gbv-consult-visit', 'store');
                 // Route::put('patient-gbv-consult/{patientGbvLegalCase}', 'update');
+            });
+        Route::controller(\App\Http\Controllers\API\V1\GenderBasedViolence\PatientGbvUserController::class)
+            ->middleware(('auth:api'))
+            ->group(function () {
+                Route::get('patient-gbv-user', 'index');
             });
     });
 
