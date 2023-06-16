ea7390cd3716b49dfdb342de89c1848b945c2aaf
Emmanuel Perez <noel@Noel-2.local>
2023-06-16 16:09:37 +0800

    Updated konsulta case number command

--- app/Console/Commands/GenerateKonsultaCodeCommand.php
+++ app/Console/Commands/GenerateKonsultaCodeCommand.php
@@ -28,31 +28,14 @@ class GenerateKonsultaCodeCommand extends Command
      */
     public function handle()
     {
-
         $data = new Patient;
-
-        echo $tableName = $data->getTable();
+        $tableName = $data->getTable();
         $patients = $data->whereNull('case_number')->withWhereHas('philhealthKonsulta')->get();
         $patients->map(function($patient) use($tableName){
-//            echo $credential = PhilhealthCredential::query()
-//                ->select('accreditation_number')
-//                ->where('facility_code', $patient->facility_code)
-//                ->whereProgramCode('kp')
-//                ->first();
             $credential = $patient->philhealthKonsulta->pluck('accreditation_number')->first();
-            //echo $credential->accreditation_number;
             $prefix = 'T'.$credential.$patient->created_at->format('Ym');
             $caseNumber = IdGenerator::generate(['table' => $tableName, 'field' => 'case_number', 'length' => 21, 'prefix' => $prefix, 'reset_on_prefix_change' => true]);
             $patient->update(['case_number' => $caseNumber]);
         });
-
-        //$prefix = 'T'.auth()->user()->konsultaCredential->accreditation_number.date('Ym');
-        //$caseNumber = IdGenerator::generate(['table' => $tableName, 'field' => 'case_number', 'length' => 21, 'prefix' => $prefix, 'reset_on_prefix_change' => true]);
-//        $model->case_number = $caseNumber;
-//        $this->table(
-//            ['Last Name', 'First Name', 'Middle Name'],
-//            $data
-//        );
-//        echo $data->count();
     }
 }
