a0284bfbc4e950d1a6d678764738406046cf1b5b
Mark Santos <christian.santos37@gmail.com>
2023-06-19 14:02:02 +0800

    add new library for gbv medical impressions:

---
+++ database/migrations/2023_06_19_110544_create_lib_gbv_medical_impressions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lib_gbv_medical_impressions', function (Blueprint $table) {
+            $table->id();
+            $table->string('desc');
+            $table->text('full_desc');
+            $table->unsignedInteger('sequence');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lib_gbv_medical_impressions');
+    }
+};
