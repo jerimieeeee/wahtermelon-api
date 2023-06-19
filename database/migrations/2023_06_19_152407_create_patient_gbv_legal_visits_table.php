5f61f2c2fdfe7c33e0d4fec6707d68d5b35898ac
Mark Santos <christian.santos37@gmail.com>
2023-06-19 16:00:59 +0800

    add new table for legal visits

---
+++ database/migrations/2023_06_19_152407_create_patient_gbv_legal_visits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patient_gbv_legal_visits', function (Blueprint $table) {
+            $table->ulid('id')->index()->primary();
+            $table->foreignUuid('patient_id')->index()->constrained();
+            $table->foreignUuid('user_id')->index()->constrained();
+            $table->string('facility_code')->index();
+            $table->foreignUlid('patient_gbv_intake_id')->constrained();
+            $table->date('scheduled_date')->nullable();
+            $table->date('actual_date')->nullable();
+            $table->string('person_in_charge')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('facility_code')->references('code')->on('facilities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patient_gbv_legal_visits');
+    }
+};
