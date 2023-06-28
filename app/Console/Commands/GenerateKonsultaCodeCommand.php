f5486d78e64c94646223fac87f5c23c1f2488ddc
Emmanuel Perez <noel@Noel-2.local>
2023-06-28 15:53:15 +0800

    Added command for generating konsulta transaction number in patient philhealth table

--- app/Console/Commands/GenerateKonsultaCodeCommand.php
+++ app/Console/Commands/GenerateKonsultaCodeCommand.php
@@ -54,17 +54,41 @@ class GenerateKonsultaCodeCommand extends Command
 
         $philhealth = new PatientPhilhealth;
         $philhealthTableName = $philhealth->getTable();
-        $philhealth->whereIn('id', function ($query) use($philhealthTableName){
-            $query->select('id')
-                ->from(function ($subquery) use ($philhealthTableName) {
-                    $subquery->selectRaw("SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(id ORDER BY created_at DESC), ',', 1), ',', -1) AS id")
-                        ->from($philhealthTableName)
-                        ->whereNull('transmittal_number')
-                        ->groupBy('patient_id', 'effectivity_year')
-                        ->havingRaw('count(patient_id) > 1');
-                });
-        })->delete();
+//        $philhealth->whereIn('id', function ($query) use($philhealthTableName){
+//            $query->select('id')
+//                ->from(function ($subquery) use ($philhealthTableName) {
+//                    $subquery->selectRaw("SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(id ORDER BY created_at DESC), ',', 1), ',', -1) AS id")
+//                        ->from($philhealthTableName)
+//                        ->whereNull('transmittal_number')
+//                        ->groupBy('patient_id', 'effectivity_year')
+//                        ->havingRaw('count(patient_id) > 1');
+//                });
+//        })->delete();
         //echo $philhealth->get();
+        do {
+            // Retrieve the duplicate records
+            $duplicateRecords = $philhealth->whereIn('id', function ($query) use ($philhealthTableName) {
+                $query->select('id')
+                    ->from(function ($subquery) use ($philhealthTableName) {
+                        $subquery->selectRaw("SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(id ORDER BY created_at DESC), ',', 1), ',', -1) AS id")
+                            ->from($philhealthTableName)
+                            ->whereNull('transmittal_number')
+                            ->groupBy('patient_id', 'effectivity_year')
+                            ->havingRaw('count(patient_id) > 1');
+                    });
+            })->get();
 
+            if ($duplicateRecords->count() > 0) {
+                // Delete the duplicate records
+                $philhealth->whereIn('id', $duplicateRecords->pluck('id'))->delete();
+            }
+        } while ($duplicateRecords->count() > 0);
+        $philhealthData = $philhealth->whereNull('transaction_number')->withWhereHas('patient.philhealthKonsulta')->get();
+        $philhealthData->map(function($philhealth) use($philhealthTableName){
+            $credential = $philhealth->patient->philhealthKonsulta->pluck('accreditation_number')->first();
+            $prefix = $credential.$philhealth->created_at->format('Ym');
+            $transactionNumber = IdGenerator::generate(['table' => $philhealthTableName, 'field' => 'transaction_number', 'length' => 21, 'prefix' => $prefix, 'reset_on_prefix_change' => true]);
+            $philhealth->update(['transaction_number' => $transactionNumber]);
+        });
     }
 }
