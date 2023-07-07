6fe73deb75228d0cd46fb90388d81a4499185090
Emmanuel Perez <noel@Noel-2.local>
2023-07-07 16:37:22 +0800

    Added patient migration

--- app/Console/Commands/MigrateMisuWahCommand.php
+++ app/Console/Commands/MigrateMisuWahCommand.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class MigrateMisuWahCommand extends Command
 {
@@ -80,13 +81,26 @@ class MigrateMisuWahCommand extends Command
         $bar->finish();
 
         $patients = $this->migratePatient();
+        $this->info("Creating progress bar...\n");
         $bar = $this->output->createProgressBar(count($patients));
-
+        //$bar->setFormat("%message%\n %current%/%max% [%bar%] %percent:3s%%");
+        $bar->setFormatDefinition('custom', '%countdown% [%bar%]');
+        $bar->setFormat('custom');
+        $bar->setPlaceholderFormatter('countdown', function (ProgressBar $progressBar) {
+            return $progressBar->getMaxSteps() - $progressBar->getProgress();
+        });
+        //$bar->setMessage("100? I won't count all that!");
+        //$bar->setProgress(0);
         $bar->start();
-        $patients->collect()->chunk(200)->each(function ($patient) use($connectionName, $database, $bar){
+        $patients->collect()->chunk(200)->each(function ($patient, $chunkNumber) use($connectionName, $database, $bar){
             $values = [];
             //$updateColumns = ['value']; // Define the columns to update if a conflict occurs
             //DB::purge($connectionName);
+            $startIndex = $chunkNumber * 200 + 1;
+            $endIndex = min($startIndex + $patient->count() - 1, $startIndex + 200 - 1);
+
+            $bar->setMessage("Processing chunk range: $startIndex - $endIndex");
+
             foreach ($patient as $record) {
                 //echo $values[] = $record;
                 //dd((array) $record);
@@ -121,82 +135,12 @@ class MigrateMisuWahCommand extends Command
                 //echo DB::table('user')->get();
                 $bar->advance();
             }
+
+            //echo $key;
             //$model->upsert($values, ['custom_id']);
         });
-        /*foreach ($databaseNames as $database) {
-            //$databaseName = $database->Database;
-            $connectionName = 'mysql_migration';
-            $this->migrationConnection($connectionName, $database);
-
-            //$results = DB::connection($connectionName)->table('your_table')->select('*')->get();
-
-            //$results = DB::connection($connectionName)->table('patient')->select('*')->whereNull('wahtermelon_patient_id')->get();
-            //$results = DB::connection($connectionName)->table('user')->select('*')->get();
-            $misuwahUsers = DB::connection('mysql_migration')->table('user')->selectRaw('
-                    id,
-                    last_name,
-                    first_name,
-                    middle_name,
-                    CASE
-                        WHEN suffix_name = "NOTAP"
-                        THEN "NA"
-                        WHEN suffix_name IS NULL OR suffix_name = ""
-                        THEN "NA"
-                        ELSE suffix_name
-                    END suffix_name,
-                    birthdate,
-                    gender,
-                    email,
-                    mobile_number AS contact_number,
-                    CASE
-                        WHEN is_active = "Y"
-                        THEN 1
-                        ELSE 0
-                    END is_active,
-                    designation AS designation_code,
-                    employer AS employer_code,
-                    tin_number,
-                    accreditation_number,
-                    created_at,
-                    updated_at
-                ')
-                ->where('email', '!=', '')
-                ->get()->collect()->chunk(200);
-            // Perform your operations on each matching database here
-            // ...
-            //DB::purge($connectionName);
-            //echo User::query()->get();
-            $misuwahUsers->each(function ($user) use($connectionName, $database){
-                $values = [];
-                //$updateColumns = ['value']; // Define the columns to update if a conflict occurs
-                //DB::purge($connectionName);
-                foreach ($user as $record) {
-                    //echo $values[] = $record;
-                    //dd((array) $record);
-                    $record = (array) $record;
-                    if(!$record['employer_code']){
-                        Arr::pull($record, 'employer_code');
-                    }
-                    $newUser = User::updateOrCreate([
-                        'last_name' => $record['last_name'],
-                        'first_name' => $record['first_name'],
-                        'middle_name' => $record['middle_name'],
-                        'suffix_name' => $record['suffix_name'],
-                        'email' => $record['email']
-                    ],
-                        $record + ['password' => Str::password(8), 'facility_code' => $database]);
-                    //$user->update(['wahtermelon_user_id' => $newUser->id]);
-                    //Update the misuwah user database with wahtermelon_user_id from wahtermelon database
-                    DB::connection($connectionName)->table('user')->whereId($record['id'])->update(['wahtermelon_user_id' => $newUser->id]);
-                    $this->components->twoColumnDetail($record['id'], $newUser->id);
-                    //echo DB::table('user')->get();
-                }
-
-
-                //$model->upsert($values, ['custom_id']);
-            });
-            //echo $misuwahUsers;
-        }*/
+        $bar->setMessage("Migrated!");
+        $bar->finish();
     }
 
     public function migrationConnection($connectionName, $database)
@@ -206,16 +150,16 @@ class MigrateMisuWahCommand extends Command
 
         DB::purge($connectionName); // Clear any previous configurations for the connection
 
-// Retrieve the database connection configuration array
+        // Retrieve the database connection configuration array
         $config = config("database.connections.$connectionName");
 
-// Update the 'database' parameter with the new database name
+        // Update the 'database' parameter with the new database name
         $config['database'] = $newDatabaseName;
 
-// Set the updated configuration for the connection
+        // Set the updated configuration for the connection
         $connection = app(ConnectionFactory::class)->make($config);
 
-// Set the new connection instance for the specific connection name
+        // Set the new connection instance for the specific connection name
         DB::connection($connectionName)->setPdo($connection->getPdo())->setReadPdo($connection->getReadPdo());
         // Add column if it doesn't exist on the 'patient' table
         // Add column if it doesn't exist on the 'patient' table
