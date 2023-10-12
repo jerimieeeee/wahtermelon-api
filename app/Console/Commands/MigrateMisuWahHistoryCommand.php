2cf660ded6e962e7bbeb976dfddb256d72c53ec6
Emmanuel Perez <noel@Noel-2.local>
2023-10-12 14:53:54 +0800

    Added patient vitals migration

--- app/Console/Commands/MigrateMisuWahHistoryCommand.php
+++ app/Console/Commands/MigrateMisuWahHistoryCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\V1\Patient\PatientVitals;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Database\Connectors\ConnectionFactory;
@@ -40,7 +41,7 @@ class MigrateMisuWahHistoryCommand extends Command
         $this->migrationConnection($connectionName, $database);
 
         $vitals = $this->getVitals();
-        $this->saveVitals($vitals);
+        $this->saveVitals($vitals, $database);
     }
 
     public function migrationConnection($connectionName, $database)
@@ -81,6 +82,65 @@ class MigrateMisuWahHistoryCommand extends Command
     public function getVitals()
     {
         return DB::connection('mysql_migration')->table('consult_vitals')
+            ->selectRaw('
+                vitals_id AS id,
+                consult.wahtermelon_consult_id AS consult_id,
+                patient.wahtermelon_patient_id AS patient_id,
+                user.wahtermelon_user_id AS user_id,
+                patient.birthdate AS birthdate,
+                vitals_date,
+                CASE
+                    WHEN vitals_weight = 0
+                    THEN NULL
+                    ELSE vitals_weight
+                END AS patient_weight,
+                CASE
+                    WHEN vitals_height = 0
+                    THEN NULL
+                    ELSE vitals_height
+                END AS patient_height,
+                CASE
+                    WHEN vitals_temp = 0
+                    THEN NULL
+                    ELSE vitals_temp
+                END AS patient_temp,
+                CASE
+                    WHEN vitals_systolic = 0
+                    THEN NULL
+                    ELSE vitals_systolic
+                END AS bp_systolic,
+                CASE
+                    WHEN vitals_diastolic = 0
+                    THEN NULL
+                    ELSE vitals_diastolic
+                END AS bp_diastolic,
+                CASE
+                    WHEN vitals_heartrate = 0
+                    THEN NULL
+                    ELSE vitals_heartrate
+                END AS patient_heart_rate,
+                CASE
+                    WHEN vitals_resprate = 0
+                    THEN NULL
+                    ELSE vitals_resprate
+                END AS patient_respiratory_rate,
+                CASE
+                    WHEN vitals_pulse = 0
+                    THEN NULL
+                    ELSE vitals_pulse
+                END AS patient_pulse_rate,
+                CASE
+                    WHEN vitals_waist = 0
+                    THEN NULL
+                    ELSE vitals_waist
+                END AS patient_waist,
+                CASE
+                    WHEN vitals_muac = 0
+                    THEN NULL
+                    ELSE vitals_muac
+                END AS patient_muac,
+                vitals_muac AS patient_muac
+            ')
             ->join('patient', function ($join) {
                 $join->on('consult_vitals.patient_id', '=', 'patient.id')
                     ->whereNotNull('patient.wahtermelon_patient_id');
@@ -89,11 +149,15 @@ class MigrateMisuWahHistoryCommand extends Command
                 $join->on('consult_vitals.user_id', '=', 'user.id')
                     ->whereNotNull('user.wahtermelon_user_id');
             })
+            ->leftJoin('consult', function ($join) {
+                $join->on('consult_vitals.consult_id', '=', 'consult.id')
+                    ->whereNotNull('consult.wahtermelon_consult_id');
+            })
             ->whereNull('wahtermelon_vitals_id')
             ->get();
     }
 
-    public function saveVitals($vitals)
+    public function saveVitals($vitals, $facilityCode)
     {
         $vitalsCount = count($vitals);
         if ($vitalsCount < 1) {
@@ -112,11 +176,30 @@ class MigrateMisuWahHistoryCommand extends Command
 
         foreach ($chunks as $chunk) {
             foreach ($chunk as $vitalsData) {
-                //dd($vitalsData->birthdate);
                 $years = Carbon::parse($vitalsData->birthdate)->diffInYears($vitalsData->vitals_date);
                 $months = Carbon::parse($vitalsData->birthdate)->diffInMonths($vitalsData->vitals_date);
-                dd($months);
+                $vitalsData = (array) $vitalsData;
+                $vitalsData['patient_age_years'] = $years;
+                $vitalsData['patient_age_months'] = $months;
+                unset($vitalsData['birthdate']);
+                //dd($vitalsData);
+                DB::transaction(function () use ($vitalsData, $facilityCode) {
+                    $vitals = PatientVitals::query()
+                        ->updateOrCreate(['patient_id' => $vitalsData['patient_id'], 'vitals_date' => $vitalsData['vitals_date']], $vitalsData + ['facility_code' => $facilityCode]);
+                    DB::connection('mysql_migration')->table('consult_vitals')->where('vitals_id', $vitalsData['id'])->update(['wahtermelon_vitals_id' => $vitals->id]);
+
+                });
+
+                $vitalsBar->advance();
             }
         }
+        $vitalsBar->finish();
+        $endTime = time();
+        $elapsedTime = $endTime - $startTime;
+
+        $this->newLine();
+        $this->components->twoColumnDetail('Patient Vitals Migration', 'Done');
+        $this->newLine();
+        $this->line('Elapsed Time: ' . gmdate('H:i:s', $elapsedTime));
     }
 }
