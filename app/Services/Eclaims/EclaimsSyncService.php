8539008d895080ff536143f23a972b41d544f319
Jerimie Garcia <je@Jerimie.local>
2023-10-16 16:49:04 +0800

    edit code on eclaimsSyncService

--- app/Services/Eclaims/EclaimsSyncService.php
+++ app/Services/Eclaims/EclaimsSyncService.php
@@ -60,40 +60,5 @@ class EclaimsSyncService
         } else {
             throw new \Exception('All servers are not working');
         }
-
-//        $urls = [
-//            'https://eclaimslive.philhealth.gov.ph:8077/SOAP',
-//            'https://eclaimslive1.philhealth.gov.ph:8077/SOAP',
-//            'https://eclaimslive2.philhealth.gov.ph:8077/SOAP',
-//            'https://eclaimslive3.philhealth.gov.ph:8077/SOAP',
-//            'https://eclaimslive4.philhealth.gov.ph:8077/SOAP',
-//            'https://eclaimslive5.philhealth.gov.ph:8077/SOAP',
-//        ];
-//
-//        $client = new Client();
-//
-//        $onlineUrls = [];
-//
-//        foreach ($urls as $url) {
-//            // Check if the URL can be resolved
-//            $host = parse_url($url, PHP_URL_HOST);
-//            $ip = gethostbyname($host);
-//
-//            if ($ip !== $host) {
-//                try {
-//                    $response = $client->get($url);
-//
-//                    if ($response->getStatusCode() === 200) {
-//                        $onlineUrls[] = $url;
-////                        dd($onlineUrls);
-//                    }
-//                } catch (RequestException $e) {
-////                    return $onlineUrls;
-//                }
-//            } else {
-////                return $onlineUrls;
-//            }
-//        }
-//        return $onlineUrls;
     }
 }
