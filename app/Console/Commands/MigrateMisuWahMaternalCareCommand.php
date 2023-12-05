9a7e5cb0d833dc186970be85ff4f29bb10d5d371
Emmanuel Perez <noel@Noel-2.local>
2023-12-05 16:15:54 +0800

    Added maternal care migration

--- app/Console/Commands/MigrateMisuWahMaternalCareCommand.php
+++ app/Console/Commands/MigrateMisuWahMaternalCareCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\V1\MaternalCare\PatientMc;
 use App\Models\V1\MaternalCare\PatientMcPreRegistration;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Database\Connectors\ConnectionFactory;
 use Illuminate\Database\Schema\Blueprint;
@@ -85,7 +86,15 @@ class MigrateMisuWahMaternalCareCommand extends Command
             ->selectRaw('
                 patient_mc.*
             ')
-            ->addSelect('patient.wahtermelon_patient_id AS patient_id', 'user.wahtermelon_user_id AS user_id')
+            ->addSelect(
+                'patient.wahtermelon_patient_id AS patient_id',
+                'user.wahtermelon_user_id AS user_id',
+                DB::raw('
+                    CASE WHEN healthy_baby = "Y" THEN 1 ELSE 0 END AS healthy_baby,
+                    CASE WHEN breastfeeding = "Y" THEN 1 ELSE 0 END AS breastfeeding,
+                    CASE WHEN end_pregnancy = "Y" THEN 1 ELSE 0 END AS end_pregnancy,
+                    CASE WHEN delivery_location IS NOT NULL THEN delivery_location ELSE "HC" END AS delivery_location_code
+                '))
             ->join('patient', function ($join) {
                 $join->on('patient_mc.patient_id', '=', 'patient.id')
                     ->whereNotNull('patient.wahtermelon_patient_id');
@@ -124,8 +133,61 @@ class MigrateMisuWahMaternalCareCommand extends Command
                     if (empty($patientMcData['pregnancy_termination_code'])) {
                         unset($patientMcData['pregnancy_termination_code']);
                     }
+
                     if(!empty($patientMcData['pre_registration_date'])) {
-                        $pre = PatientMc::query()
+
+                        $patientMcData['edc_date'] = Carbon::parse($patientMcData['edc_date'])->addDays(280)->format('Y-m-d');
+                        $mapping = [
+                            'brgy_code' => 'barangay_code',
+                            'outcome_id' => 'outcome_code',
+                            'attendant_id' => 'attendant_code',
+                            // Add more mappings as needed
+                        ];
+
+                        foreach ($mapping as $oldKey => $newKey) {
+                            if (isset($patientMcData[$oldKey])) {
+                                $patientMcData[$newKey] = $patientMcData[$oldKey];
+                                unset($patientMcData[$oldKey]);
+                            }
+                        }
+
+                        $keysToRemove = [
+                            'mc_id',
+                            'wahtermelon_mc_id',
+                            'patient_id',
+                            'post_registration_date',
+                            'admission_date',
+                            'discharge_date',
+                            'delivery_date',
+                            'delivery_location',
+                            'delivery_location_code',
+                            'barangay_code',
+                            'gravidity',
+                            'parity',
+                            'full_term',
+                            'preterm',
+                            'abortion',
+                            'livebirths',
+                            'outcome_code',
+                            'healthy_baby',
+                            'birth_weight',
+                            'attendant_code',
+                            'breastfeeding',
+                            'breastfed_date',
+                            'end_pregnancy',
+                            'postpartum_remarks',
+                            'patient_age',
+                            'patient_height',
+                            'pregnancy_termination_date',
+                            'pregnancy_termination_cause',
+                            'pregnancy_termination_code'
+                        ];
+
+                        $patientMcDataWithoutMcId = collect($patientMcData)
+                            ->except($keysToRemove)
+                            ->toArray();
+
+                        $mc = PatientMc::query()
                             ->where('patient_id', $patientMcData['patient_id'])
                             ->where('pregnancy_termination_date', $patientMcData['pregnancy_termination_date'])
                             ->where('created_at', $patientMcData['created_at'])
@@ -133,13 +195,18 @@ class MigrateMisuWahMaternalCareCommand extends Command
                                 $query->where('pre_registration_date', $patientMcData['pre_registration_date'])
                                     ->where('lmp_date', $patientMcData['lmp_date']);
                             })->first();
-                        if($pre) {
-                            $pre->preRegister->updateOrCreate();
+                        if($mc) {
+                            $mc->preRegister()->updateOrCreate($patientMcDataWithoutMcId, $patientMcDataWithoutMcId + ['facility_code' => $facilityCode]);
                         } else {
+                            $mc = PatientMc::query()->updateOrCreate(['patient_id' => $patientMcData['patient_id'], 'patient_age' => $patientMcData['patient_age'], 'created_at' => $patientMcData['created_at']], $patientMcData + ['facility_code' => $facilityCode]);
+                            $mc->preRegister()->updateOrCreate($patientMcDataWithoutMcId, $patientMcDataWithoutMcId + ['facility_code' => $facilityCode]);
+                        }
 
+                        if(!empty($patientMcData['post_registration_date']) && !empty($patientMcData['admission_date'])) {
+                            $mc->postRegister()->updateOrCreate(['patient_mc_id' => $mc->id], $patientMcData + ['facility_code' => $facilityCode]);
                         }
                     }
-                    $mc = PatientMc::query()->updateOrCreate(['patient_id' => $patientMcData['patient_id'], 'patient_age' => $patientMcData['patient_age'], 'created_at' => $patientMcData['created_at']], $patientMcData + ['facility_code' => $facilityCode]);
+
 //                    $surgical = PatientSurgicalHistory::query()->updateOrCreate(['patient_id' => $surgicalHistoryData['patient_id']], $surgicalHistoryData + ['facility_code' => $facilityCode]);
                     if ($mc) {
                         DB::connection('mysql_migration')->table('patient_mc')->where('mc_id', $patientMcData['mc_id'])->update(['wahtermelon_mc_id' => $mc->id]);
