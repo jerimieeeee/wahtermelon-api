8903dc72a219abf26c4757adb4eb40ca2b6f3d7e
Mark Santos <christian.santos37@gmail.com>
2024-01-02 16:26:10 +0800

    throw catch immidiately

--- app/Services/Eclaims/EclaimsSyncService.php
+++ app/Services/Eclaims/EclaimsSyncService.php
@@ -56,11 +56,11 @@ class EclaimsSyncService
                 break;
             } catch (\SoapFault | \Exception $e) {
                 // Handle the exception if needed, or continue to the next URL.
-                if($e->getCode() === 0) {
+                // if($e->getCode() === 0) {
 
-                } else {
+                // } else {
                     throw $e;
-                }
+                // }
             }
         }
 
