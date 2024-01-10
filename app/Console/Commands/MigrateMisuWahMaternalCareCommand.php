d2b0cb1a7a2a812d468c50bd10c8aae2fead7829
Emmanuel Perez <noel@Noel-2.local>
2024-01-10 09:05:01 +0800

    Added maternal care migration

--- app/Console/Commands/MigrateMisuWahMaternalCareCommand.php
+++ app/Console/Commands/MigrateMisuWahMaternalCareCommand.php
@@ -93,7 +93,8 @@ class MigrateMisuWahMaternalCareCommand extends Command
                     CASE WHEN healthy_baby = "Y" THEN 1 ELSE 0 END AS healthy_baby,
                     CASE WHEN breastfeeding = "Y" THEN 1 ELSE 0 END AS breastfeeding,
                     CASE WHEN end_pregnancy = "Y" THEN 1 ELSE 0 END AS end_pregnancy,
-                    CASE WHEN delivery_location IS NOT NULL THEN delivery_location ELSE "HC" END AS delivery_location_code
+                    CASE WHEN delivery_location IS NOT NULL THEN delivery_location ELSE "HC" END AS delivery_location_code,
+                    CASE WHEN birth_weight IS NOT NULL THEN birth_weight ELSE "0" END AS birth_weight
                 '))
             ->join('patient', function ($join) {
                 $join->on('patient_mc.patient_id', '=', 'patient.id')
@@ -108,7 +109,23 @@ class MigrateMisuWahMaternalCareCommand extends Command
             ->get();
     }
 
-    public function savePatientMc($patientMc, $facilityCode)
+    public function getMcRisk($mcId)
+    {
+        return DB::connection('mysql_migration')->table('consult_mc_risk')
+            ->join('patient', function ($join) {
+                $join->on('consult_mc_risk.patient_id', '=', 'patient.id')
+                    ->whereNotNull('patient.wahtermelon_patient_id');
+            })
+            ->join('user AS user', function ($join) {
+                $join->on('consult_mc_risk.user_id', '=', 'user.id')
+                    ->whereNotNull('user.wahtermelon_user_id');
+            })
+            ->where('mc_id', $mcId)
+            //->whereNull('wahtermelon_mc_id')
+            ->get();
+    }
+
+    /*public function savePatientMc($patientMc, $facilityCode)
     {
         $patientMcCount = count($patientMc);
         if ($patientMcCount < 1) {
@@ -202,7 +219,7 @@ class MigrateMisuWahMaternalCareCommand extends Command
                             $mc->preRegister()->updateOrCreate($patientMcDataWithoutMcId, $patientMcDataWithoutMcId + ['facility_code' => $facilityCode]);
                         }
 
-                        if(!empty($patientMcData['post_registration_date']) && !empty($patientMcData['admission_date'])) {
+                        if(!empty($patientMcData['post_registration_date']) && !empty($patientMcData['admission_date']) && !empty($patientMcData['barangay_code'])) {
                             $mc->postRegister()->updateOrCreate(['patient_mc_id' => $mc->id], $patientMcData + ['facility_code' => $facilityCode]);
                         }
                     }
@@ -223,5 +240,186 @@ class MigrateMisuWahMaternalCareCommand extends Command
         $this->components->twoColumnDetail('Patient Maternal Care Migration', 'Done');
         $this->newLine();
         $this->line('Elapsed Time: ' . gmdate('H:i:s', $elapsedTime));
+    }*/
+
+    public function savePatientMc($patientMc, $facilityCode)
+    {
+        $patientMcCount = count($patientMc);
+        if ($patientMcCount < 1) {
+            $this->components->info('Nothing to migrate');
+            return;
+        }
+
+        $this->processPatientMc($patientMc, $facilityCode);
+
+        $this->newLine();
+        $this->components->twoColumnDetail('Patient Maternal Care Migration', 'Done');
     }
+
+    /**
+     * Process Patient Maternal Care
+     *
+     * @param $patientMc
+     * @param $facilityCode
+     * @return void
+     */
+    private function processPatientMc($patientMc, $facilityCode): void
+    {
+        $patientMcBar = $this->output->createProgressBar(count($patientMc));
+        $patientMcBar->setFormat('Processing Patient Maternal Care Table: %current%/%max% [%bar%] %percent:3s%% Elapsed: %elapsed:6s% Remaining: %remaining:6s% Estimated: %estimated:-6s%');
+        $patientMcBar->start();
+        $startTime = time();
+
+        $this->chunkAndProcess($patientMc, $facilityCode);
+
+        $patientMcBar->finish();
+        $this->displayElapsedTime($startTime);
+    }
+
+    /**
+     * Chunk and Process Patient Mc Data
+     *
+     * @param $patientMc
+     * @param $facilityCode
+     * @return void
+     */
+    private function chunkAndProcess($patientMc, $facilityCode): void
+    {
+        $chunkSize = 200;
+        $chunks = array_chunk($patientMc->toArray(), $chunkSize);
+
+        foreach ($chunks as $chunk) {
+            foreach ($chunk as $patientMcData) {
+                $this->processPatientMcData($patientMcData, $facilityCode);
+            }
+        }
+    }
+
+    /**
+     * Process Patient Mc Data
+     *
+     * @param $patientMcData
+     * @param $facilityCode
+     * @return void
+     */
+    private function processPatientMcData($patientMcData, $facilityCode): void
+    {
+        DB::transaction(function () use ($patientMcData, $facilityCode) {
+            $patientMcData = (array) $patientMcData;
+            $success = false;
+
+            try {
+                if (empty($patientMcData['pregnancy_termination_code'])) {
+                    unset($patientMcData['pregnancy_termination_code']);
+                }
+
+                if (!empty($patientMcData['pre_registration_date'])) {
+
+                    $patientMcData['edc_date'] = Carbon::parse($patientMcData['edc_date'])->addDays(280)->format('Y-m-d');
+                    $mapping = [
+                        'brgy_code' => 'barangay_code',
+                        'outcome_id' => 'outcome_code',
+                        'attendant_id' => 'attendant_code',
+                        // Add more mappings as needed
+                    ];
+
+                    foreach ($mapping as $oldKey => $newKey) {
+                        if (isset($patientMcData[$oldKey])) {
+                            $patientMcData[$newKey] = $patientMcData[$oldKey];
+                            unset($patientMcData[$oldKey]);
+                        }
+                    }
+
+                    $keysToRemove = [
+                        'mc_id',
+                        'wahtermelon_mc_id',
+                        'patient_id',
+                        'post_registration_date',
+                        'admission_date',
+                        'discharge_date',
+                        'delivery_date',
+                        'delivery_location',
+                        'delivery_location_code',
+                        'barangay_code',
+                        'gravidity',
+                        'parity',
+                        'full_term',
+                        'preterm',
+                        'abortion',
+                        'livebirths',
+                        'outcome_code',
+                        'healthy_baby',
+                        'birth_weight',
+                        'attendant_code',
+                        'breastfeeding',
+                        'breastfed_date',
+                        'end_pregnancy',
+                        'postpartum_remarks',
+                        'patient_age',
+                        'patient_height',
+                        'pregnancy_termination_date',
+                        'pregnancy_termination_cause',
+                        'pregnancy_termination_code'
+                    ];
+
+                    $patientMcDataWithoutMcId = collect($patientMcData)
+                        ->except($keysToRemove)
+                        ->toArray();
+
+//                    $mc = PatientMc::query()
+//                        ->where('patient_id', $patientMcData['patient_id'])
+//                        ->where('pregnancy_termination_date', $patientMcData['pregnancy_termination_date'])
+//                        ->where('created_at', $patientMcData['created_at'])
+//                        ->whereHas('preRegister', function ($query) use ($patientMcData) {
+//                            $query->where('pre_registration_date', $patientMcData['pre_registration_date'])
+//                                ->where('lmp_date', $patientMcData['lmp_date']);
+//                        })->first();
+//                    //dd('a');
+//                    if ($mc) {
+//
+//                        $mc->preRegister()->updateOrCreate($patientMcDataWithoutMcId, $patientMcDataWithoutMcId + ['facility_code' => $facilityCode]);
+//                        if (!empty($patientMcData['post_registration_date']) && !empty($patientMcData['admission_date']) && !empty($patientMcData['barangay_code'])) {
+//                            $mc->postRegister()->updateOrCreate(['patient_mc_id' => $mc->id], $patientMcData + ['facility_code' => $facilityCode]);
+//                        }
+//                    } else {
+                        //$mc = PatientMc::query()->create(['patient_id' => $patientMcData['patient_id'], 'patient_age' => $patientMcData['patient_age'], 'created_at' => $patientMcData['created_at']], $patientMcData + ['facility_code' => $facilityCode]);
+                        $mc = PatientMc::query()->create($patientMcData + ['facility_code' => $facilityCode]);
+                        $mc->preRegister()->create($patientMcDataWithoutMcId + ['facility_code' => $facilityCode]);
+                        if (!empty($patientMcData['post_registration_date']) && !empty($patientMcData['admission_date']) && !empty($patientMcData['barangay_code'])) {
+                            $mc->postRegister()->create($patientMcData + ['facility_code' => $facilityCode]);
+                        }
+
+                        dd($this->getMcRisk($patientMcData['mc_id']));
+//                    }
+//
+//                    if (!empty($patientMcData['post_registration_date']) && !empty($patientMcData['admission_date']) && !empty($patientMcData['barangay_code'])) {
+//                        $mc->postRegister()->updateOrCreate(['patient_mc_id' => $mc->id], $patientMcData + ['facility_code' => $facilityCode]);
+//                    }
+
+                    $success = true;
+                }
+            } catch (\Exception $e) {
+
+            }
+
+            if ($success) {
+                DB::connection('mysql_migration')->table('patient_mc')->where('mc_id', $patientMcData['mc_id'])->update(['wahtermelon_mc_id' => $mc->id]);
+            }
+        });
+    }
+
+    /**
+     * Display Elapsed Time
+     *
+     * @param int $startTime
+     * @return void
+     */
+    private function displayElapsedTime(int $startTime): void
+    {
+        $endTime = time();
+        $elapsedTime = $endTime - $startTime;
+        $this->newLine();
+        $this->line('Elapsed Time: ' . gmdate('H:i:s', $elapsedTime));
+    }
+
 }
