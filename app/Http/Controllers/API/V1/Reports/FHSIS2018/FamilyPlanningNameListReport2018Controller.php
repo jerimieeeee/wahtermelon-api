ba0ec307f9b95343d3002af93d5903b6dc092868
Jerimie Garcia <je@Jerimie.local>
2024-04-04 15:35:02 +0800

    add fp namelist

---
+++ app/Http/Controllers/API/V1/Reports/FHSIS2018/FamilyPlanningNameListReport2018Controller.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Reports\FHSIS2018;
+
+use App\Http\Controllers\Controller;
+use App\Services\FamilyPlanning\ReportFamilyPlanningNameListService;
+use Illuminate\Http\Request;
+
+class FamilyPlanningNameListReport2018Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request, ReportFamilyPlanningNameListService $fpNamelistService)
+    {
+        return $fpNamelistService->get_report_namelist($request)->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
