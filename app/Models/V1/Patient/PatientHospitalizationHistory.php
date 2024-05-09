886722f3348e3c331c349b1d2419cb8143fb16af
Mark Santos <christian.santos37@gmail.com>
2024-05-09 08:45:26 +0800

    add patient hospitalization

---
+++ app/Models/V1/Patient/PatientHospitalizationHistory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\V1\Patient;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use App\Traits\FilterByUser;
+use DateTimeInterface;
+
+class PatientHospitalizationHistory extends Model
+{
+    use HasFactory, HasUlids, FilterByUser;
+
+    protected $table = 'patient_hospitalization_histories';
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    // public function getRouteKeyName()
+    // {
+    //     return 'patient_id';
+    // }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class, 'facility_code', 'code');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+}
---
+++ database/migrations/2024_05_07_153439_create_patient_hospitalization_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patient_hospitalization_histories', function (Blueprint $table) {
+            $table->ulid('id')->index()->primary();
+            $table->foreignUuid('patient_id')->index()->constrained();
+            $table->foreignUuid('user_id')->index()->constrained();
+            $table->string('facility_code')->index();
+            $table->string('cause');
+            $table->date('admission_date');
+            $table->date('discharge_date');
+            $table->timestamps();
+
+            $table->foreign('facility_code')->references('code')->on('facilities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patient_hospitalization_histories');
+    }
+};
