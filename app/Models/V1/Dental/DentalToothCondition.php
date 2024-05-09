477455e33e367010b27e393bfd2e20624cf3c68d
Mark Santos <christian.santos37@gmail.com>
2024-05-09 08:49:11 +0800

    add hospitalizatin history model

---
+++ app/Models/V1/Dental/DentalToothCondition.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\V1\Dental;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DentalToothCondition extends Model
+{
+    use HasFactory;
+}
