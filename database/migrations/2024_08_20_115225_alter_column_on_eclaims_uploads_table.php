451c7bf3ba0e60ff78d90108c05160f668f1d757
Mark Santos <christian.santos37@gmail.com>
2024-08-20 14:48:28 +0800

    update migration

--- database/migrations/2024_08_20_115225_alter_column_on_eclaims_uploads_table.php
+++ database/migrations/2024_08_20_115225_alter_column_on_eclaims_uploads_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::table('eclaims_uploads', function (Blueprint $table) {
             $table->json('denied_reason')->change();
         });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
