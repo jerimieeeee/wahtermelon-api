7c7f63a7b24731b140347ea36d620b480dda155d
Mark Santos <christian.santos37@gmail.com>
2024-11-20 13:59:09 +0800

    update medine route and regimen list

--- database/seeders/LibMedicineDoseRegimenSeeder.php
+++ database/seeders/LibMedicineDoseRegimenSeeder.php
@@ -15,14 +15,15 @@ class LibMedicineDoseRegimenSeeder extends Seeder
     public function run()
     {
         LibMedicineDoseRegimen::upsert([
-            ['code' => 'OD', 'sequence' => 1, 'desc' => 'Once a day'],
+            ['code' => 'OD',  'sequence' => 1, 'desc' => 'Once a day'],
             ['code' => 'BID', 'sequence' => 2, 'desc' => '2 x a day - Every 12 hours'],
             ['code' => 'TID', 'sequence' => 3, 'desc' => '3 x a day - Every 8 hours'],
             ['code' => 'QID', 'sequence' => 4, 'desc' => '4 x a day - Every 6 hours'],
-            ['code' => 'QOD', 'sequence' => 5, 'desc' => 'Every other day'],
-            ['code' => 'QHS', 'sequence' => 6, 'desc' => 'Every bedtime'],
-            ['code' => 'OTH', 'sequence' => 7, 'desc' => 'Others'],
-            ['code' => 'PRN', 'sequence' => 7, 'desc' => 'As needed (Pro Re Nata)'],
+            ['code' => 'HID', 'sequence' => 5, 'desc' => '6 x a day - Every 4 hours'],
+            ['code' => 'QOD', 'sequence' => 6, 'desc' => 'Every other day'],
+            ['code' => 'QHS', 'sequence' => 7, 'desc' => 'Every bedtime'],
+            ['code' => 'OTH', 'sequence' => 8, 'desc' => 'Others'],
+            ['code' => 'PRN', 'sequence' => 9, 'desc' => 'As needed (Pro Re Nata)'],
         ], ['code']);
     }
 }
