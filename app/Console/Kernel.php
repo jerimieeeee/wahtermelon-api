53d7d3787182f9173d560da27acba71ec90b8057
Emmanuel Perez <noel@Emmanuels-MacBook-Air.local>
2025-01-13 15:19:30 +0800

    Updated schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,10 +29,10 @@ class Kernel extends ConsoleKernel
 
         })*/
         $schedule->command('eclaims:status')
-        ->weeklyOn(6, '15:16') // Every Saturday at 8:00 PM (6 is Saturday, 20:00 is 8:00 PM)
+        ->weeklyOn(6, '15:31') // Every Saturday at 8:00 PM (6 is Saturday, 20:00 is 8:00 PM)
         ->timezone('Asia/Manila'); // Set the timezone to Asia/Manila
 
-        $schedule->command('clean:household-folders')->dailyAt('15:15')->timezone('Asia/Manila');
+        $schedule->command('clean:household-folders')->dailyAt('15:30')->timezone('Asia/Manila');
     }
 
     /**
