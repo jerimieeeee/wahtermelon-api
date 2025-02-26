a541a70d9b582e46b09f90209a871979c557eae5
Jerimie Garcia <je@Jerimie.local>
2025-02-26 10:49:30 +0800

    added vaccine_status on patients table

---
+++ app/Console/Commands/ParseVaccineStatusCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ParseVaccineStatusCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'vaccine-status:parse';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_02_26_094932_add_column_on_patients_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('patients', function (Blueprint $table) {
+            $table->char('vaccine_status', 10)->after('consent_flag')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('patients', function (Blueprint $table) {
+            $table->dropIndex(['vaccine_status']);
+            $table->dropColumn('vaccine_status');
+        });
+    }
+};
